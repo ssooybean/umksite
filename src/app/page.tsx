010364efe9190e5fbfdadd4fffb3554eefa9fee9
Ivakina Alina <93323532+ssooybean@users.noreply.github.com>
2024-01-25 12:42:25 +0300

    alina-start-build-preloader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import CasesList from "@/widgets/Cases/CasesList";
 import IntroText from "@/widgets/Introduce/IntroText";
+import Preloader from "@/widgets/Preloader/Preloader";
 
 export default function Home() {
   return (
     <main className="flex justify-between relative ">
+      <Preloader />
       <section className="container m-auto w-full justify-between h-[100vh] flex items-center">
         <div>3d</div>
         <IntroText />
